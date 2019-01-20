8aa0dcfdcdf68037004fbf27183709e06d608e17
franckentien <franckentien@hotmail.fr>
2019-01-20 17:11:32 +0100

    Add interaction with buy Store button

--- Assets/Scripts/store.cs
+++ Assets/Scripts/store.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
 using UnityEngine;
 
-public class store : MonoBehaviour {
+/// <summary>
+/// The store.
+/// </summary>
+public class Store : MonoBehaviour
+{
+    /// <summary>
+    /// The store count.
+    /// </summary>
+    private int storeCount;
 
-	// Use this for initialization
-	void Start () {
+    /// <summary>
+    /// The buy store on click.
+    /// </summary>
+    public void BuyStoreOnClick()
+    {
+        this.storeCount += 1;
+        Debug.Log(this.storeCount);
+    }
+
+    /// <summary>
+    /// The start.
+    /// Use this for initialization
+    /// </summary>
+    private void Start ()
+    {
+        this.storeCount = 0;
+    }
+
+    /// <summary>
+    /// The update.
+    /// Update is called once per frame
+    /// </summary>
+    private void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
 }
